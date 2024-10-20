efbfff2fda4c6e799ef4d8b85278b14839aae56a
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-10-20 22:51:36 +0200

    formatted, fixed chart

--- graphs.py
+++ graphs.py
@@ -7,9 +7,13 @@ gpgi_long = pd.read_csv("data/gpgi_long.csv")
 
 def create_bubble_lifeexp():
     pop = pd.read_csv("init_data/Total_population.csv")
-    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][["LOCATION", "TIME", "Value"]]
+    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][
+        ["LOCATION", "TIME", "Value"]
+    ]
     new_ind = pd.read_csv("init_data/Life_expectancy.csv")
-    new_ind = new_ind.loc[new_ind["Indicator"] == "Life expectancy at birth, total (years)"][["Indicator", "LOCATION", "TIME", "Value"]]
+    new_ind = new_ind.loc[
+        new_ind["Indicator"] == "Life expectancy at birth, total (years)"
+    ][["Indicator", "LOCATION", "TIME", "Value"]]
     gpgi = pd.read_csv("data/gpgi_long.csv")
 
     df = gpgi.merge(pop, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"])
@@ -20,16 +24,38 @@ def create_bubble_lifeexp():
     df.rename(columns={"Value": "life_expectancy"}, inplace=True)
     df.drop(columns=["TIME", "LOCATION"], inplace=True)
 
-
     df["pop_thousands"] = df["pop_thousands"] * 1000
     df = df[["life_expectancy", "gpgi", "pop_thousands", "region", "country", "year"]]
-    df.columns = ["Očekávaná průměrná délka života", "GPGI", "Populace", "Region", "Země", "Rok"]
+    df.columns = [
+        "Očekávaná průměrná délka života",
+        "GPGI",
+        "Populace",
+        "Region",
+        "Země",
+        "Rok",
+    ]
 
-    fig = px.scatter(df, x="Očekávaná průměrná délka života", y="GPGI", size="Populace", color="Region", hover_name="Země", template="none", labels={"Očekávaná průměrná délka života": "Očekávaná průměrná délka života (log)", "GPGI":"GPGI"}, size_max=60, log_x=True, animation_frame="Rok", animation_group="Země")
+    fig = px.scatter(
+        df,
+        x="Očekávaná průměrná délka života",
+        y="GPGI",
+        size="Populace",
+        color="Region",
+        hover_name="Země",
+        template="none",
+        labels={
+            "Očekávaná průměrná délka života": "Očekávaná průměrná délka života (log)",
+            "GPGI": "GPGI",
+        },
+        size_max=60,
+        log_x=True,
+        animation_frame="Rok",
+        animation_group="Země",
+    )
 
     fig.update_layout(
         title={
-            'text': "GPGI vs HDP",
+            "text": "GPGI vs Očekávaná průměrná délka života",
         },
         font=dict(
             # family="Poppins, sans-serif",
@@ -37,7 +63,7 @@ def create_bubble_lifeexp():
         ),
         hoverlabel=dict(
             font_size=16,
-        )
+        ),
     )
 
     # fig.update_layout(transition = {'duration': 3000})
@@ -48,9 +74,13 @@ def create_bubble_lifeexp():
 
 def create_bubble_gdp():
     pop = pd.read_csv("init_data/Total_population.csv")
-    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][["LOCATION", "TIME", "Value"]]
-    new_ind = pd.read_csv("init_data/Life_expectancy.csv")
-    new_ind = new_ind.loc[new_ind["Indicator"] == "Life expectancy at birth, total (years)"][["Indicator", "LOCATION", "TIME", "Value"]]
+    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][
+        ["LOCATION", "TIME", "Value"]
+    ]
+    new_ind = pd.read_csv("init_data/GDP_per_capita.csv")
+    new_ind = new_ind.loc[
+        new_ind["Indicator"] == "GDP per capita, PPP (current international $)"
+    ][["Indicator", "LOCATION", "TIME", "Value"]]
     gpgi = pd.read_csv("data/gpgi_long.csv")
 
     df = gpgi.merge(pop, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"])
@@ -58,19 +88,34 @@ def create_bubble_gdp():
     df.drop(columns=["TIME", "LOCATION"], inplace=True)
 
     df = df.merge(new_ind, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"])
-    df.rename(columns={"Value": "life_expectancy"}, inplace=True)
+    df.rename(columns={"Value": "gdp"}, inplace=True)
     df.drop(columns=["TIME", "LOCATION"], inplace=True)
 
-
     df["pop_thousands"] = df["pop_thousands"] * 1000
-    df = df[["life_expectancy", "gpgi", "pop_thousands", "region", "country", "year"]]
-    df.columns = ["Očekávaná průměrná délka života", "GPGI", "Populace", "Region", "Země", "Rok"]
+    df = df[["gdp", "gpgi", "pop_thousands", "region", "country", "year"]]
+    df.columns = ["HDP", "GPGI", "Populace", "Region", "Země", "Rok"]
 
-    fig = px.scatter(df, x="Očekávaná průměrná délka života", y="GPGI", size="Populace", color="Region", hover_name="Země", template="none", labels={"Očekávaná průměrná délka života": "Očekávaná průměrná délka života (log)", "GPGI":"GPGI"}, size_max=60, log_x=True, animation_frame="Rok", animation_group="Země")
+    fig = px.scatter(
+        df,
+        x="HDP",
+        y="GPGI",
+        size="Populace",
+        color="Region",
+        hover_name="Země",
+        template="none",
+        labels={
+            "Očekávaná průměrná délka života": "Očekávaná průměrná délka života (log)",
+            "GPGI": "GPGI",
+        },
+        size_max=60,
+        log_x=True,
+        animation_frame="Rok",
+        animation_group="Země",
+    )
 
     fig.update_layout(
         title={
-            'text': "GPGI vs HDP",
+            "text": "GPGI vs HDP",
         },
         font=dict(
             # family="Poppins, sans-serif",
@@ -78,18 +123,20 @@ def create_bubble_gdp():
         ),
         hoverlabel=dict(
             font_size=16,
-        )
+        ),
     )
 
     # fig.update_layout(transition = {'duration': 3000})
-    fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 1500
-    
+    fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 1000
+
     return fig
 
 
 def bubble_school_exp_female():
     pop = pd.read_csv("init_data/Total_population.csv")
-    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][["LOCATION", "TIME", "Value"]]
+    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][
+        ["LOCATION", "TIME", "Value"]
+    ]
     gpgi = pd.read_csv("data/gpgi_long.csv")
 
     df = gpgi.merge(pop, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"])
@@ -100,24 +147,48 @@ def bubble_school_exp_female():
     school_female = pd.read_csv("init_data/School_life_expectancy_female.csv")
 
     # Filter and rename for consistency
-    school_male = school_male.loc[school_male['Indicator'] == "School life expectancy, primary to tertiary, male (years)"][["LOCATION", "TIME", "Value"]]
-    school_female = school_female.loc[school_female['Indicator'] == "School life expectancy, primary to tertiary, female (years)"][["LOCATION", "TIME", "Value"]]
+    school_male = school_male.loc[
+        school_male["Indicator"]
+        == "School life expectancy, primary to tertiary, male (years)"
+    ][["LOCATION", "TIME", "Value"]]
+    school_female = school_female.loc[
+        school_female["Indicator"]
+        == "School life expectancy, primary to tertiary, female (years)"
+    ][["LOCATION", "TIME", "Value"]]
 
     school_male.rename(columns={"Value": "School_Expectancy_Male"}, inplace=True)
     school_female.rename(columns={"Value": "School_Expectancy_Female"}, inplace=True)
 
     # Merge the datasets
-    df = df.merge(school_male, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"], how='left')
-    df = df.merge(school_female, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"], how='left')
+    df = df.merge(
+        school_male, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"], how="left"
+    )
+    df = df.merge(
+        school_female,
+        left_on=["iso3", "year"],
+        right_on=["LOCATION", "TIME"],
+        how="left",
+    )
     # df.drop(columns=["TIME", "LOCATION"], inplace=True)
 
     # Visualize the data
-    fig = px.scatter(df, x="School_Expectancy_Female", y="gpgi", size="pop_thousands", color="region",
-                    hover_name="country", template="none", labels={"HDP": "HDP (log)"}, size_max=60, log_x=True,
-                    animation_frame="year", animation_group="country")
+    fig = px.scatter(
+        df,
+        x="School_Expectancy_Female",
+        y="gpgi",
+        size="pop_thousands",
+        color="region",
+        hover_name="country",
+        template="none",
+        labels={"HDP": "HDP (log)"},
+        size_max=60,
+        log_x=True,
+        animation_frame="year",
+        animation_group="country",
+    )
 
     fig.update_layout(
-        title='GDPI vs Průměrná délka školní docházky - Ženy',
+        title="GDPI vs Průměrná délka školní docházky - Ženy",
         xaxis_title="Průměrná délka školní docházky - Ženy (log)",
         yaxis_title="GPGI",
     )
@@ -127,7 +198,9 @@ def bubble_school_exp_female():
 
 def bubble_school_exp_male():
     pop = pd.read_csv("init_data/Total_population.csv")
-    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][["LOCATION", "TIME", "Value"]]
+    pop = pop.loc[pop["Indicator"] == "Total population (thousands)"][
+        ["LOCATION", "TIME", "Value"]
+    ]
     gpgi = pd.read_csv("data/gpgi_long.csv")
 
     df = gpgi.merge(pop, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"])
@@ -138,47 +211,91 @@ def bubble_school_exp_male():
     school_female = pd.read_csv("init_data/School_life_expectancy_female.csv")
 
     # Filter and rename for consistency
-    school_male = school_male.loc[school_male['Indicator'] == "School life expectancy, primary to tertiary, male (years)"][["LOCATION", "TIME", "Value"]]
-    school_female = school_female.loc[school_female['Indicator'] == "School life expectancy, primary to tertiary, female (years)"][["LOCATION", "TIME", "Value"]]
+    school_male = school_male.loc[
+        school_male["Indicator"]
+        == "School life expectancy, primary to tertiary, male (years)"
+    ][["LOCATION", "TIME", "Value"]]
+    school_female = school_female.loc[
+        school_female["Indicator"]
+        == "School life expectancy, primary to tertiary, female (years)"
+    ][["LOCATION", "TIME", "Value"]]
 
     school_male.rename(columns={"Value": "School_Expectancy_Male"}, inplace=True)
     school_female.rename(columns={"Value": "School_Expectancy_Female"}, inplace=True)
 
     # Merge the datasets
-    df = df.merge(school_male, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"], how='left')
-    df = df.merge(school_female, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"], how='left')
+    df = df.merge(
+        school_male, left_on=["iso3", "year"], right_on=["LOCATION", "TIME"], how="left"
+    )
+    df = df.merge(
+        school_female,
+        left_on=["iso3", "year"],
+        right_on=["LOCATION", "TIME"],
+        how="left",
+    )
     # df.drop(columns=["TIME", "LOCATION"], inplace=True)
 
     # Visualize the data
-    fig = px.scatter(df, x="School_Expectancy_Male", y="gpgi", size="pop_thousands", color="region",
-                    hover_name="country", template="none", labels={"HDP": "HDP (log)"}, size_max=60, log_x=True,
-                    animation_frame="year", animation_group="country")
+    fig = px.scatter(
+        df,
+        x="School_Expectancy_Male",
+        y="gpgi",
+        size="pop_thousands",
+        color="region",
+        hover_name="country",
+        template="none",
+        labels={"HDP": "HDP (log)"},
+        size_max=60,
+        log_x=True,
+        animation_frame="year",
+        animation_group="country",
+    )
 
     fig.update_layout(
-        title='GDPI vs Průměrná délka školní docházky - Muži',
+        title="GDPI vs Průměrná délka školní docházky - Muži",
         xaxis_title="Průměrná délka školní docházky - Muži (log)",
         yaxis_title="GPGI",
     )
-    
+
     return fig
 
 
 def dem_vyvoj_hdp(country):
     df = pd.read_csv("init_data/GDP_per_capita.csv")
-    df = df.loc[df["Indicator"] == "GDP per capita, PPP (current international $)"][["Indicator", "LOCATION", "TIME", "Value", "Country"]]
+    df = df.loc[df["Indicator"] == "GDP per capita, PPP (current international $)"][
+        ["Indicator", "LOCATION", "TIME", "Value", "Country"]
+    ]
 
     df = df.loc[df["Country"] == country]
-    fig = px.line(df, x="TIME", y="Value", title=f"GDP per capita - {country}", template="none", labels={"TIME": "Rok", "Value":"HDP"})
+    fig = px.line(
+        df,
+        x="TIME",
+        y="Value",
+        title=f"GDP per capita - {country}",
+        template="none",
+        labels={"TIME": "Rok", "Value": "HDP"},
+    )
 
     return fig
 
 
 def top_10graph():
     gpgi_sorted = gpgi_long.sort_values(by="year", ascending=True)
-    gpgi_sorted = gpgi_long.sort_values(by="rank", ascending=True)[["gpgi", "country", "region", "year"]]
+    gpgi_sorted = gpgi_long.sort_values(by="rank", ascending=True)[
+        ["gpgi", "country", "region", "year"]
+    ]
     gpgi_sorted.dropna(subset=["gpgi"], inplace=True)
 
-    fig = px.bar(gpgi_sorted, x="gpgi", y="country", orientation="h", range_y=[0,20], animation_frame="year", animation_group="country", labels="")
+    fig = px.bar(
+        gpgi_sorted,
+        x="gpgi",
+        y="country",
+        orientation="h",
+        range_y=[0, 20],
+        animation_frame="year",
+        animation_group="country",
+        labels="",
+    )
 
     fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 1500
 
@@ -186,22 +303,36 @@ def top_10graph():
 
 
 def cz_hdp(year, country):
-    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(by="rank", ascending=True)[["rank", "country", "region"]]
+    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(
+        by="rank", ascending=True
+    )[["rank", "country", "region"]]
     gpgi_sorted.dropna(subset=["rank"], inplace=True)
 
     df = pd.read_csv("init_data/GDP_per_capita.csv")
-    df = df.loc[df["Indicator"] == "GDP per capita, PPP (current international $)"][["Indicator", "LOCATION", "TIME", "Value", "Country"]]
+    df = df.loc[df["Indicator"] == "GDP per capita, PPP (current international $)"][
+        ["Indicator", "LOCATION", "TIME", "Value", "Country"]
+    ]
     df.columns = ["Indicator", "LOCATION", "TIME", "Value", "Země"]
 
     df = df.loc[(df["Země"].isin([country, "Czechia"]))]
 
-    fig = px.line(df, x="TIME", y="Value", title=f"HDP - {country}", template="none", color="Země", labels={"TIME":"Rok", "Value":"HDP"})
+    fig = px.line(
+        df,
+        x="TIME",
+        y="Value",
+        title=f"HDP - {country}",
+        template="none",
+        color="Země",
+        labels={"TIME": "Rok", "Value": "HDP"},
+    )
 
     return fig
 
 
 def cz_rural(year, country):
-    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(by="rank", ascending=True)[["rank", "country", "region"]]
+    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(
+        by="rank", ascending=True
+    )[["rank", "country", "region"]]
     gpgi_sorted.dropna(subset=["rank"], inplace=True)
 
     top10 = gpgi_sorted.head(10).country
@@ -209,11 +340,20 @@ def cz_rural(year, country):
     top10
 
     df = pd.read_csv("init_data/Rural_population.csv")
-    df = df.loc[df["Indicator"] == "Rural population (% of total population)"][["Indicator", "LOCATION", "TIME", "Value", "Country"]]
+    df = df.loc[df["Indicator"] == "Rural population (% of total population)"][
+        ["Indicator", "LOCATION", "TIME", "Value", "Country"]
+    ]
 
     df = df.loc[(df["Country"].isin([country, "Czechia"]))]
 
+    fig = px.line(
+        df,
+        x="TIME",
+        y="Value",
+        title=f"Rural population",
+        template="none",
+        color="Country",
+        labels={"Value": "Podíl obyvatelstva, žijícího na venkově (%)", "TIME": "Rok"},
+    )
 
-    fig = px.line(df, x="TIME", y="Value", title=f"Rural population", template="none", color="Country", labels={"Value":"Podíl obyvatelstva, žijícího na venkově (%)", "TIME": "Rok"})
-    
     return fig
--- main.py
+++ main.py
@@ -19,163 +19,179 @@ scope_values = {
     "Severní Amerika": "north america",
     "Jižní Amerika": "south america",
     "Asie": "asia",
-    "Afrika":"africa"
+    "Afrika": "africa",
 }
 
 tab1, tab2, tab3 = st.tabs(["Tabulky", "GPGI", "ČR"])
 
 with tab1:
-   st.header("Data")
-   st.write("Přehled zpracovaných dat.")
+    st.header("Data")
+    st.write("Přehled zpracovaných dat.")
 
-   col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
 
-   st.subheader("GPGI")
-   st.write("Gender pay gap index v letech 2016–2023.")
+    st.subheader("GPGI")
+    st.write("Gender pay gap index v letech 2016–2023.")
 
-   gpgi_country = st.selectbox("Země", ["Vše"] + gpgi["Country"].unique().tolist())
-   
-   if not gpgi_country == "Vše":
-       st.dataframe(gpgi.loc[gpgi["Country"] == gpgi_country])
-   else:
-       st.dataframe(gpgi)
-   
-   st.subheader("Demografické ukazatele")
-   
-   col1, col2 = st.columns(2)
-   
-   data_country = col1.selectbox("Země", ["Vše"] + data["Country"].unique().tolist())
-   indicator = col2.selectbox("Ukazatel", ["Vše"] + data["Indicator"].unique().tolist())
-   
-   if (data_country == "Vše") and (indicator == "Vše"):
-       st.dataframe(data, use_container_width=True)
-   elif indicator == "Vše":
-       st.dataframe(data.loc[data["Country"] == data_country], use_container_width=True)
-   elif data_country == "Vše":
-       st.dataframe(data.loc[data["Indicator"] == indicator], use_container_width=True)
-   else:
-       st.dataframe(data.loc[(data["Indicator"] == indicator) & (data["Country"] == data_country)], use_container_width=True)
+    gpgi_country = st.selectbox("Země", ["Vše"] + gpgi["Country"].unique().tolist())
+
+    if not gpgi_country == "Vše":
+        st.dataframe(gpgi.loc[gpgi["Country"] == gpgi_country])
+    else:
+        st.dataframe(gpgi)
+
+    st.subheader("Demografické ukazatele")
+
+    col1, col2 = st.columns(2)
+
+    data_country = col1.selectbox("Země", ["Vše"] + data["Country"].unique().tolist())
+    indicator = col2.selectbox(
+        "Ukazatel", ["Vše"] + data["Indicator"].unique().tolist()
+    )
+
+    if (data_country == "Vše") and (indicator == "Vše"):
+        st.dataframe(data, use_container_width=True)
+    elif indicator == "Vše":
+        st.dataframe(
+            data.loc[data["Country"] == data_country], use_container_width=True
+        )
+    elif data_country == "Vše":
+        st.dataframe(data.loc[data["Indicator"] == indicator], use_container_width=True)
+    else:
+        st.dataframe(
+            data.loc[
+                (data["Indicator"] == indicator) & (data["Country"] == data_country)
+            ],
+            use_container_width=True,
+        )
 
 with tab2:
     st.header("Vývoj GPGI v letech 2016 až 2023")
     col1, col2 = st.columns(2)
-    
-    year = col1.selectbox("Rok",[year for year in range(2016, 2024)])
+
+    year = col1.selectbox("Rok", [year for year in range(2016, 2024)])
     scope = col2.selectbox("Území", scope_values.keys())
-    
-    col1, col2, col3 = st.columns([2,1,1])
+
+    col1, col2, col3 = st.columns([2, 1, 1])
 
     # Create a choropleth map
-    fig = px.choropleth(gpgi, locations="ISO3", locationmode='ISO-3',
-                        color=f"{year} Rank",
-                        hover_name="Country",
-                        color_continuous_scale=px.colors.sequential.Plasma_r,
-                        scope=scope_values[scope]
-                        )
+    fig = px.choropleth(
+        gpgi,
+        locations="ISO3",
+        locationmode="ISO-3",
+        color=f"{year} Rank",
+        hover_name="Country",
+        color_continuous_scale=px.colors.sequential.Plasma_r,
+        scope=scope_values[scope],
+    )
     if scope_values[scope] == "world":
         fig.update_geos(
             projection_type="natural earth",
             # showcoastlines=True, coastlinecolor="RebeccaPurple",
-            showland=True, landcolor="LightGreen",
-            showocean=True, oceancolor="LightBlue"
+            showland=True,
+            landcolor="LightGreen",
+            showocean=True,
+            oceancolor="LightBlue",
         )
 
-
     fig.update_layout(
-        title={
-            'y':0.95,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'
-        },
-        font=dict(
-            size=18,
-            color="Black"
-        ),
-        hoverlabel=dict(
-            bgcolor="white",
-            font_size=16,
-            font_family="Rockwell"
-        )
-        
+        title={"y": 0.95, "x": 0.5, "xanchor": "center", "yanchor": "top"},
+        font=dict(size=18, color="Black"),
+        hoverlabel=dict(bgcolor="white", font_size=16, font_family="Rockwell"),
     )
 
-    fig.update_layout(
-        margin=dict(l=0, r=0, t=50, b=0)
-    )
+    fig.update_layout(margin=dict(l=0, r=0, t=50, b=0))
 
     fig.update_layout(title_text="")
     col1.plotly_chart(fig)
-    
-    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(by="rank", ascending=True)[["rank", "country", "region"]]
+
+    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(
+        by="rank", ascending=True
+    )[["rank", "country", "region"]]
     gpgi_sorted.dropna(subset=["rank"], inplace=True)
     gpgi_sorted.columns = ["Pořadí", "Země", "Region"]
-    
+
     col2.subheader("Prvních 10")
     col2.dataframe(gpgi_sorted.head(10), hide_index=True)
-    
-    col3.subheader("Posledních 10", help="Zobrazeny jsou pouze státy, kde byla dostupná data")
+
+    col3.subheader(
+        "Posledních 10", help="Zobrazeny jsou pouze státy, kde byla dostupná data"
+    )
     col3.dataframe(gpgi_sorted.tail(10), hide_index=True)
-    
+
     st.plotly_chart(graphs.top_10graph())
-    
+
     top10 = gpgi_sorted.head(10)["Země"].unique()
-    
+
     st.divider()
-    
+
     st.header("Demografické charakteristiky top 10 států")
-    
+
     country = st.selectbox("Země", top10)
-    
+
     st.plotly_chart(graphs.dem_vyvoj_hdp(country))
-    
+
     st.divider()
-    
+
     st.header("Porovnání GPGI s vybranými demografickými ukazateli")
-    
-    # indicator = st.selectbox("ad",["HDP", "Očekávaná průměrná délka života", "Průměrná délka školní docházky - Ženy", "Průměrná délka školní docházky - Muži"])    
-    tab_hdp, tab_lifeexp, tab_schoolexpm, tab_schoolexpf = st.tabs(["HDP", "Očekávaná průměrná délka života", "Průměrná délka školní docházky - Muži", "Průměrná délka školní docházky - Ženy"])
-    
+
+    # indicator = st.selectbox("ad",["HDP", "Očekávaná průměrná délka života", "Průměrná délka školní docházky - Ženy", "Průměrná délka školní docházky - Muži"])
+    tab_hdp, tab_lifeexp, tab_schoolexpm, tab_schoolexpf = st.tabs(
+        [
+            "HDP",
+            "Očekávaná průměrná délka života",
+            "Průměrná délka školní docházky - Muži",
+            "Průměrná délka školní docházky - Ženy",
+        ]
+    )
+
     with tab_hdp:
-        st.plotly_chart(graphs.create_bubble_gdp())
+        st.plotly_chart(graphs.create_bubble_gdp(), key="gdp")
     with tab_lifeexp:
-        st.plotly_chart(graphs.create_bubble_lifeexp())
+        st.plotly_chart(graphs.create_bubble_lifeexp(), key="lifeexp")
     with tab_schoolexpm:
-        st.plotly_chart(graphs.bubble_school_exp_male())
+        st.plotly_chart(graphs.bubble_school_exp_male(), key="schoolexpm")
     with tab_schoolexpf:
-        st.plotly_chart(graphs.bubble_school_exp_female())
+        st.plotly_chart(graphs.bubble_school_exp_female(), key="schoolexpf")
 
 with tab3:
     st.header("Čeká Republika")
     st.subheader("Vývoj GPGI v České republice")
-    fig = px.line(gpgi_long.loc[gpgi_long["country"] == "Czech Republic"], x="year", y="gpgi", template="none", labels={"year": "Rok", "gpgi":"GPGI"})
+    fig = px.line(
+        gpgi_long.loc[gpgi_long["country"] == "Czech Republic"],
+        x="year",
+        y="gpgi",
+        template="none",
+        labels={"year": "Rok", "gpgi": "GPGI"},
+    )
 
     fig.update_layout(
-        font=dict(
-            color="Black"
-        ),
-        hoverlabel=dict(
-            bgcolor="white",
-            font_size=16,
-            font_family="Rockwell"
-        )
+        font=dict(color="Black"),
+        hoverlabel=dict(bgcolor="white", font_size=16, font_family="Rockwell"),
     )
 
     st.plotly_chart(fig)
-    
+
     st.divider()
-    
+
     st.subheader("Porovnání ČR s ostatními státy")
-    
-    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(by="rank", ascending=True)[["rank", "country", "region"]]
+
+    gpgi_sorted = gpgi_long.loc[(gpgi_long["year"] == year)].sort_values(
+        by="rank", ascending=True
+    )[["rank", "country", "region"]]
     gpgi_sorted.dropna(subset=["rank"], inplace=True)
 
     top10 = gpgi_sorted.head(10).country
 
     col1, col2 = st.columns(2)
-    
-    year = col1.selectbox("Rok",[year for year in range(2016, 2024)], key="asdakjsdnm", help="Rok, který určuje 10 států s nejvyšším GPGI")
+
+    year = col1.selectbox(
+        "Rok",
+        [year for year in range(2016, 2024)],
+        key="asdakjsdnm",
+        help="Rok, který určuje 10 států s nejvyšším GPGI",
+    )
     country = col2.selectbox("Země", top10, key="apjsdna")
-    
-    col1.plotly_chart(graphs.cz_hdp(year,country))
+
+    col1.plotly_chart(graphs.cz_hdp(year, country))
     col2.plotly_chart(graphs.cz_rural(year, country))
